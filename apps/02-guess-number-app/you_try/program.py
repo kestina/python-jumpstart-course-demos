502d292c49fa55cc8072a81a8919cad6424732fb
Cristina <crislofdez@gmail.com>
2018-05-21 21:40:32 +0200

    change print format and fix the bug in the if
    cast to int the input of the user and then user .format in print function.
    Fix the condition in the elif.

--- apps/02-guess-number-app/you_try/program.py
+++ apps/02-guess-number-app/you_try/program.py
@@ -6,13 +6,13 @@ print('---------------------------------')
 number = random.randint(0, 100)
 user_number = 10250
 
-while int(user_number) != number:
+while user_number != number:
 
-    user_number = input('Guess a number between 0 and 100:')
+    user_number = int(input('Guess a number between 0 and 100:'))
 
-    if number == int(user_number):
-        print('YES! You\'ve got it. The number was '+str(number))
-    elif int(user_number) > number:
-        print('Sorry, but '+user_number+' is LOWER than the number')
+    if user_number == number:
+        print('YES! You\'ve got it. The number was {}'.format(number))
+    elif user_number > number:
+        print('Sorry, but {} is HIGHER than the number'.format(user_number))
     else:
-        print('Sorry, but ' + user_number + ' is HIGHER than the number')
+        print('Sorry, but {} is LOWER than the number'.format(user_number))

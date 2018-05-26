85fe14aa4aa603cb71b99341652461888d335835
Cristina <crislofdez@gmail.com>
2018-05-26 19:06:33 +0200

    Separate data from UI
    Add a new file to manage the data of the journal, save, load and add_entries.

---
+++ apps/04_journal/you_try/journal.py
@@ -0,0 +1,12 @@
+
+def load(name):
+    # todo: populate from file if it exists.
+    return []
+
+
+def save(name, journal_data):
+    pass
+
+
+def add_entry(text, data):
+    data.append(text)
--- apps/04_journal/you_try/program.py
+++ apps/04_journal/you_try/program.py
@@ -1,3 +1,4 @@
+import journal
 
 def main():
     print_header()
@@ -13,7 +14,8 @@ def print_header():
 def run_event_loop():
     print('What do you want to do with your journal?')
     cmd = None
-    journal_data = []  # list()
+    journal_name = 'default'
+    journal_data = journal.load(journal_name) # []  # list()
 
     while cmd != 'x':
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ').lower().strip()
@@ -26,6 +28,7 @@ def run_event_loop():
             print("Sorry, we don't understand '{}'.".format(cmd))
 
     print('Done, goodbye.')
+    journal.save(journal_name, journal_data)
 
 
 def list_entries(data):
@@ -37,7 +40,7 @@ def list_entries(data):
 
 def add_entry(data):
     text = input('Type your entry, <enter> to exit: ')
-    data.append(text)
+    journal.add_entry(text, data)
 
 
 main()

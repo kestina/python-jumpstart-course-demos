a13443aa2414053f50cf5c012b942883ddb1b1e1
Cristina <crislofdez@gmail.com>
2018-05-26 18:44:52 +0200

    New functions list_entries and add_entry
    Define list_entries and add_entry functions. Using lists to store the entries and a for loop to list it.

--- apps/04_journal/you_try/program.py
+++ apps/04_journal/you_try/program.py
@@ -13,18 +13,30 @@ def print_header():
 def run_event_loop():
     print('What do you want to do with your journal?')
     cmd = None
+    journal_data = []  # list()
 
     while cmd != 'x':
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ').lower().strip()
 
         if cmd == 'l':
-            print("L")
+            list_entries(journal_data)
         elif cmd == 'a':
-            print("a")
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Sorry, we don't understand '{}'.".format(cmd))
 
     print('Done, goodbye.')
+
+
+def list_entries(data):
+    print('Your journal entries: ')
+    for entry in data:
+        print(entry)
+
+
+def add_entry(data):
+    text = input('Type your entry, <enter> to exit: ')
+    data.append(text)
 
 
 main()

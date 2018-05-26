3f6c835e62b9fb849fd1f45eabe7437a15c1c657
Cristina <crislofdez@gmail.com>
2018-05-26 18:26:49 +0200

    Init App 4
    Skeleton and definition of the differents events to perform

---
+++ apps/04_journal/you_try/program.py
@@ -0,0 +1,30 @@
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print('-------------------------------')
+    print('         JOURNAL APP')
+    print('-------------------------------')
+
+
+def run_event_loop():
+    print('What do you want to do with your journal?')
+    cmd = None
+
+    while cmd != 'x':
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
+
+        if cmd == 'L':
+            print("L")
+        elif cmd == 'A':
+            print("a")
+        elif cmd != 'x':
+            print("Sorry, we don't understand '{}'.".format(cmd))
+
+    print('Done, goodbye.')
+
+
+main()

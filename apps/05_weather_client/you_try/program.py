c6ed889ecfd6059f42ee237ffe825a37bd7f7c0d
Cristina <crislofdez@gmail.com>
2018-05-29 21:09:24 +0200

    get_html_from_web v1
    Just show the web

--- apps/05_weather_client/you_try/program.py
+++ apps/05_weather_client/you_try/program.py
@@ -5,6 +5,7 @@ def main():
     code = input('What zipcode do you want the weather for (97201)?: ')
 
     # get html from web
+    get_html_from_web(code)
     # parse the html
     # display for the forecast
 
@@ -13,6 +14,11 @@ def print_the_header():
     print('----------------------------------')
     print('          WEATHER APP')
     print('----------------------------------')
+
+
+def get_html_from_web(zipcode):
+    url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
+    print(url)
 
 
 if __name__ == '__main__':

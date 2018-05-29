d058438437d2279fab047f2426ae7021d4019c6f
Cristina <crislofdez@gmail.com>
2018-05-29 20:42:38 +0200

    Init app 5
    Init app 5

---
+++ apps/05_weather_client/you_try/program.py
@@ -0,0 +1,19 @@
+
+def main():
+    print_the_header()
+
+    code = input('What zipcode do you want the weather for (97201)?: ')
+
+    # get html from web
+    # parse the html
+    # display for the forecast
+
+
+def print_the_header():
+    print('----------------------------------')
+    print('          WEATHER APP')
+    print('----------------------------------')
+
+
+if __name__ == '__main__':
+    main()

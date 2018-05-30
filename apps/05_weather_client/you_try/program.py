0be0b9ae1478c641d9acca6f2a4df9a9a8810d42
Cristina <crislofdez@gmail.com>
2018-05-30 19:51:12 +0200

    Add the functions to get all the info
    Add functions to parse and clean the html. Also use a named tuple (collections) to return all the data a format them after get it.

--- apps/05_weather_client/you_try/program.py
+++ apps/05_weather_client/you_try/program.py
@@ -1,13 +1,22 @@
+import requests
+import bs4
+import collections
+
+WeatherReport = collections.namedtuple('WeatherReport',
+                                       'cond, temp, scale, loc')
+
 
 def main():
     print_the_header()
-
     code = input('What zipcode do you want the weather for (97201)?: ')
-
-    # get html from web
-    get_html_from_web(code)
-    # parse the html
-    # display for the forecast
+    html = get_html_from_web(code)
+    report = get_weather_from_html(html)
+    print('The temp in {} is {} and {} {}'.format(
+        report.loc,
+        report.cond,
+        report.temp,
+        report.scale
+    ))
 
 
 def print_the_header():
@@ -18,7 +27,45 @@ def print_the_header():
 
 def get_html_from_web(zipcode):
     url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
-    print(url)
+    response = requests.get(url)
+    return response.text
+
+
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    # cityCss = '.region-content-header h1'
+    # weatherScaleCss = '.wu-unit-temperature .wu-label'
+    # weatherTempCss = '.wu-unit-temperature .wu-value'
+    # weatherConditionCss = '.condition-icon'
+
+    loc = soup.find(class_='region-content-header').find('h1').get_text()
+    condition = soup.find(class_='condition-icon').get_text()
+    temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
+    scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
+
+    loc = cleanup_text(loc)
+    loc = find_city_and_state_from_location(loc)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+
+    # print(condition, temp, scale, loc)
+    # return condition, temp, scale, loc
+    report = WeatherReport(cond=condition, temp=temp, scale=scale, loc=loc)
+    return report
+
+
+def find_city_and_state_from_location(loc: str):
+    parts = loc.split('\n')
+    return parts[0].strip()
+
+
+def cleanup_text(text : str):
+    if not text:
+        return text
+
+    text = text.strip()
+    return text
 
 
 if __name__ == '__main__':

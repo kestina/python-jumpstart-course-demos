e97e0152040d672da651749448a21d2c39e01d75
Cristina <crislofdez@gmail.com>
2018-06-05 20:46:50 +0200

    Class actors and model options
    It has been created class creature and class wizard, with name and level.
    Wizard has a method called attack.
    Model each option in main: attack, lookaround and runaway

--- apps/07_wizard_battle/you_try/program.py
+++ apps/07_wizard_battle/you_try/program.py
@@ -1,3 +1,8 @@
+import random
+import time
+
+from actors import Wizard
+from actors import Creature
 
 
 def main():
@@ -13,18 +18,40 @@ def print_header():
 
 
 def game_loop():
+    creatures = [
+        Creature('Toad', 1),
+        Creature('Tiger', 12),
+        Creature('Bat', 2),
+        Creature('Dragon', 50),
+        Creature('Evil Wizard', 1000)
+    ]
+
+    hero = Wizard('Gandolf', 75)
 
     while True:
 
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appear from a dark and foggy forest...'
+              .format(active_creature.name, active_creature.level))
+        print()
+
         cmd = input('Do you [a]ttach, [r]unaway, or [l]ook around? ')
         if cmd == 'a':
-            print('attack')
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!")
         elif cmd == 'r':
-            print('runaway')
+            print('The wizard has become unsure of his power and flees')
         elif cmd == 'l':
-            print('lookaround')
+            print('The wizard {} takes in the surroundings and sees: '.format(hero.name))
+            for c in creatures:
+                print('* A {} of level {}'.format(c.name, c.level))
         else:
             print("OK, exiting game... bye!")
+            break
 
 
 if __name__ == '__main__':
